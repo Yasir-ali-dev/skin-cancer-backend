2b74ae98c92aae71c5e8abd0c44e982bca5a3161
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2024-02-20 21:30:00 +0500

    get specific skin-image endoint created

--- controllers/skin-image-controller.js
+++ controllers/skin-image-controller.js
@@ -28,4 +28,39 @@ const createSkinImage = async (req, res) => {
     .json({ success: true, skin_image: skin_image });
 };
 
-module.exports = { getAllSkinImages, createSkinImage };
+const getSkinImage = async (req, res) => {
+  const image_id_ = req.params.image_id.slice(1);
+  const image = await Image.findById(image_id_);
+  if (!image) {
+    throw new NotFoundError(`image not found with id: ${image_id_}`);
+  }
+  res.status(StatusCodes.OK).json({ status: true, image: image });
+};
+
+const deleteReport = async (req, res) => {
+  const report_id_ = req.params.report_id.slice(1);
+  const report = await Report.findByIdAndDelete(report_id_);
+  if (!report) {
+    throw new NotFoundError(`report not found with id: ${report_id_}`);
+  }
+  res.status(StatusCodes.OK).json({ status: true, deleted_report: report });
+};
+const updateReport = async (req, res) => {
+  const report_id_ = req.params.report_id.slice(1);
+  if (!req.body.report_generate_date && !req.body.report_details) {
+    throw new BadRequestError(
+      "please provide report_generate_date or report_details to update"
+    );
+  }
+  console.log(report_id_);
+  const report = await Report.findByIdAndUpdate(report_id_, req.body, {
+    new: true,
+  });
+  console.log(report);
+  if (!report) {
+    throw new NotFoundError(`report not found with id: ${report_id_}`);
+  }
+  res.status(StatusCodes.OK).json({ status: true, updated_report: report });
+};
+
+module.exports = { getAllSkinImages, createSkinImage, getSkinImage };
--- routes/image-route.js
+++ routes/image-route.js
@@ -3,14 +3,13 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllSkinImages,
+  getSkinImage,
   createSkinImage,
 } = require("../controllers/skin-image-controller");
 
 router.route("/").get(getAllSkinImages).post(createSkinImage);
 
-// router
-//   .route("/:lesion_id")
-//   .get(getLesion)
+router.route("/:image_id").get(getSkinImage);
 //   .delete(deleteLesion)
 //   .patch(updateLesion);
 
