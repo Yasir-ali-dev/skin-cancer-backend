f6337414f4c826ac5338d87e77d71c535f14d255
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2024-02-20 21:48:52 +0500

    skin image crud operations created

--- controllers/skin-image-controller.js
+++ controllers/skin-image-controller.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const Image = require("../model/Image");
-const { BadRequestError } = require("../errors");
+const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllSkinImages = async (req, res) => {
   const skin_images = await Image.find({});
@@ -37,30 +37,40 @@ const getSkinImage = async (req, res) => {
   res.status(StatusCodes.OK).json({ status: true, image: image });
 };
 
-const deleteReport = async (req, res) => {
-  const report_id_ = req.params.report_id.slice(1);
-  const report = await Report.findByIdAndDelete(report_id_);
-  if (!report) {
-    throw new NotFoundError(`report not found with id: ${report_id_}`);
+const deleteImage = async (req, res) => {
+  const image_id_ = req.params.image_id.slice(1);
+  const deleted_image = await Image.findByIdAndDelete(image_id_);
+  if (!deleted_image) {
+    throw new NotFoundError(`image not found with id: ${image_id_}`);
   }
-  res.status(StatusCodes.OK).json({ status: true, deleted_report: report });
-};
-const updateReport = async (req, res) => {
-  const report_id_ = req.params.report_id.slice(1);
-  if (!req.body.report_generate_date && !req.body.report_details) {
-    throw new BadRequestError(
-      "please provide report_generate_date or report_details to update"
-    );
-  }
-  console.log(report_id_);
-  const report = await Report.findByIdAndUpdate(report_id_, req.body, {
-    new: true,
-  });
-  console.log(report);
-  if (!report) {
-    throw new NotFoundError(`report not found with id: ${report_id_}`);
-  }
-  res.status(StatusCodes.OK).json({ status: true, updated_report: report });
+  res
+    .status(StatusCodes.OK)
+    .json({ status: true, deleted_image: deleted_image });
 };
 
-module.exports = { getAllSkinImages, createSkinImage, getSkinImage };
+const updateSkinImage = async (req, res) => {
+  const image_id_ = req.params.image_id.slice(1);
+  const { image_url, image_source, captured_date } = req.body;
+  if (!image_source && !image_url && !captured_date) {
+    throw new BadRequestError(
+      "please provide image_url, image_source or captured_date to update"
+    );
+  }
+  const updated_image = await Image.findByIdAndUpdate(image_id_, req.body, {
+    new: true,
+  });
+  if (!updated_image) {
+    throw new NotFoundError(`image not found with id: ${image_id_}`);
+  }
+  res
+    .status(StatusCodes.OK)
+    .json({ status: true, updated_image: updated_image });
+};
+
+module.exports = {
+  getAllSkinImages,
+  createSkinImage,
+  getSkinImage,
+  deleteImage,
+  updateSkinImage,
+};
--- routes/image-route.js
+++ routes/image-route.js
@@ -4,13 +4,18 @@ const router = express.Router();
 const {
   getAllSkinImages,
   getSkinImage,
+
   createSkinImage,
+  deleteImage,
+  updateSkinImage,
 } = require("../controllers/skin-image-controller");
 
 router.route("/").get(getAllSkinImages).post(createSkinImage);
 
-router.route("/:image_id").get(getSkinImage);
-//   .delete(deleteLesion)
-//   .patch(updateLesion);
+router
+  .route("/:image_id")
+  .get(getSkinImage)
+  .delete(deleteImage)
+  .patch(updateSkinImage);
 
 module.exports = router;
